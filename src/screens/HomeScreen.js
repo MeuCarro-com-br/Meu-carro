e833fc89ec71adcd06613ff5a110c0283e2b1f7f
Bryan Alex Bravo Medina <bryanalexbm@gmail.com>
2022-07-20 20:50:30 -0300

    Conexão com API

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,19 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+      axios.get('/api/products').then((res) => {
+        setProducts(res.data);
+      })
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
