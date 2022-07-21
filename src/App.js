65eb23970b6fcea8bb219f9852fefd6b17ae7fdf
Bryan Alex Bravo Medina <bryanalexbm@gmail.com>
2022-07-21 20:34:44 -0300

    Instalando Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 import ProductScreen from './screens/ProductScreen';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">amazona</Link>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
+          <Container>
           <Routes>
             <Route path="/product/:slug" element={<ProductScreen />} />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
+          </Container>
         </main>
       </div>
     );
