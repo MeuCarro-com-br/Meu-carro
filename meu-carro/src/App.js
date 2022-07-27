a5a64c29719b5c5ba2b4e99feefd63b19c73c97d
Bryan Alex Bravo Medina <bryanalexbm@gmail.com>
2022-07-27 20:32:07 -0300

    Implementando usuarios

--- meu-carro/src/App.js
+++ meu-carro/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Badge from 'react-bootstrap/Badge';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import EditScreen from './screens/EditScreen';
@@ -14,10 +17,16 @@ import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
 
 function App() {
-  const { state } = useContext(Store);
-  const { cart } = state;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+  };
   return (
     <div className="d-flex flex-column site-container">
+      <ToastContainer position="bottom-center" limit={1} />
       <header>
         <Navbar variant="primary">
           <Container>
@@ -34,6 +43,28 @@ function App() {
                   </Badge>
                 )}
               </Link>
+              {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={signoutHandler}
+                    >
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
+                )}
             </Nav>
           </Container>
         </Navbar>
@@ -50,7 +81,7 @@ function App() {
         </Container>
       </main>
       <footer>
-        <div className="text-center">Direitos de autor</div>
+        <div className="text-center">Grupo 2 | Carros | Turma 15</div>
       </footer>
     </div>
   );
